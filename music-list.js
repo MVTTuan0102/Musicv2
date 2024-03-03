a911402b0da9d3e7eaaa8425a699153bad28cf07
MVTTuan0102 <133876574+MVTTuan0102@users.noreply.github.com>
2024-03-03 13:07:32 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,38 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+{
+    name: "DÒNG THỜI GIAN",
+    artist: "Nguyễn Hải Phong",
+    img: "dtg",
+    src: "dtg"
+  },
+ {
+    name: "Hẹn một mai",
+    artist: "Bùi Anh Tuấn",
+    img: "henmotmai",
+    src: "henmotmai"
+  },
+  {
+    name: "Thái Bình mồ hôi rơi",
+    artist: "Sơn Tùng M-TP",
+    img: "thaibinhmohoiroi",
+    src: "thaibinhmohoiroi"
+  },
+    {
+    name: "Chỉ Muốn Bên Em Lúc Này",
+    artist: "Huy Vạc",
+    img: "ChiMuonBenEmLucNay",
+    src: "ChiMuonBenEmLucNay"
+  }
+  ]
+  
+  
+  
