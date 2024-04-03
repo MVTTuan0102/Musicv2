3760b02af46d885389e404047403bffbc2d31f96
MVTTuan0102 <133876574+MVTTuan0102@users.noreply.github.com>
2024-04-03 13:10:06 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,93 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+ {
+    name: "Hẹn một mai",
+    artist: "Bùi Anh Tuấn",
+    img: "henmotmai",
+    src: "henmotmai"
+  },
+    {
+    name: "Thái Bình mồ hôi rơi",
+    artist: "Sơn Tùng M-TP",
+    img: "Thaibinhmohoiroi",
+    src: "Thaibinhmohoiroi"
+  },
+    {
+    name: "Ngủ ngoan em nhé",
+    artist: "Grey",
+    img: "Nd",
+    src: "Nd"
+  },
+    {
+    name: "Chạm khẽ tim anh một chút thôi",
+    artist: "NOO Phước Thịnh",
+    img: "Tn",
+    src: "Tn"
+    },    
+    {
+    name: "Gặp người đúng lúc",
+    artist: "United idol",
+    img: "gndl",
+    src: "gndl"
+    },
+    {
+    name: "Sai người sai thời điểm",
+    artist: "Thanh Hưng x MewMew",
+    img: "snstd",
+    src: "snstd"
+},
+        {
+    name: "Sau tất cả",
+    artist: "Erik",
+    img: "stc",
+    src: "stc"
+        },
+    {
+    name: "Hoa cỏ lau",
+    artist: "QN LVK",
+    img: "hoacomay",
+    src: "hoacomay"
+},
+    {
+    name: "Rồi Ta Sẽ Ngắm Pháo Hoa Cùng Nhau",
+    artist: "O lew",
+    img: "3",
+    src: "3"
+        },
+    {
+    name: "Chúng ta rồi sẽ hạnh phúc",
+    artist: "Jack - J97 x Freak D",
+    img: "4",
+    src: "4"
+},
+    {
+    name: "Cuối cùng thì",
+    artist: "Jack",
+    img: "5",
+    src: "5"
+    },
+    {
+    name: "Nắng có mang em về",
+    artist: "Shartnuss",
+    img: "ncmev",
+    src: "ncmev"
+    },
+        {
+    name: "Em ổn không",
+    artist: "Hoàng Duyên cover",
+    img: "Emonkhong",
+    src: "Emonkhong"
+    },
+    
+  ];
+  
+  
+  
